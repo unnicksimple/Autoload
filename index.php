dcd2d1ca9ad4eb0549fac76176fee5a4cb4193ef
lurugardo <1827020+unnicksimple@users.noreply.github.com>
2018-12-14 16:58:30 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+#  
+#       ___       _______    ______     ______    __  ___  __       ___     
+#      /   \     |       \  /  __  \   /  __  \  |  |/  / |  |     /   \    
+#     /  ^  \    |  .--.  ||  |  |  | |  |  |  | |  '  /  |  |    /  ^  \   
+#    /  /_\  \   |  |  |  ||  |  |  | |  |  |  | |    <   |  |   /  /_\  \  
+#   /  _____  \  |  '--'  ||  `--'  | |  `--'  | |  .  \  |  |  /  _____  \ 
+#  /__/     \__\ |_______/  \______/   \______/  |__|\__\ |__| /__/     \__\
+#  Desarrollo de webapp y diseño web || www.adookia.com || hola@adookia.com
+#
+# c[_] by @unNickSimple
+#
+# Luis Suárez
+# Programador
+#
+# Me lo contaron y lo olvidé
+# lo vi y lo entendí
+# lo hice y lo aprendí
+# Confucio, 551 - 479 ac.
+*/
+
+
+
+// Autocarga de clases a través de los namespace
+require_once 'autocarga.php';
+$p = new controladores\Prueba;
